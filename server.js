a74ddacd3be0f4331f67722c434e5a06954d3df1
sajjal <nairsajjal@gmail.com>
2019-07-03 10:12:37 +0530

    invoice router created

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ require('./models/db');
 
 const express = require('express');
 
+const invoiceController = require('./controllers/invoiceController');
+
 var app = express();
 
 app.listen(3000, ()=>{
     console.log("Server created at port 3000");
 })
+
+app.use('/invoice',invoiceController);
