f8e7cdb21bad3b3d032e11c7d9e16bfcd389b4c6
sajjal <nairsajjal@gmail.com>
2019-07-03 16:11:41 +0530

    tenative connection to database

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var router = express.Router();
-
+const mongoose = require('mongoose');
+const Invoice = mongoose.model('Invoice');
 
 router.get('/', (req,res)=> {
     res.render("invoices/addOrEdit",{
@@ -12,7 +13,36 @@ router.post('/',(req,res)=>{
    insertRecord(req,res);
 } );
 
-function insertRecord(req, res){
 
+
+function insertRecord(req, res){
+    var invoice = new Invoice();
+    invoice.invoice_number= req.body.invoice_number;
+    invoice.invoice_from= req.body.invoice_from;
+    invoice.invoice_to= req.body.invoice_to;
+    invoice.date = req.body.date;
+    invoice.due_date = req.body.due_date;
+    invoice.sub_total = req.body.sub_total;
+    invoice.discounts = req.body.discounts;
+    invoice.tax = req.body.tax;
+    invoice.shipping = req.body.shipping;
+    invoice.total = req.body.total;
+    invoice.items_id = req.body.items_id;
+    invoice.types = req.body.types;
+    invoice.quantity = req.body.quantity;
+    invoice.amount = req.body.amount;
+    invoice.price = req.body.price;
+    invoice.save((err,doc) => {
+        if(!err)
+            res.redirect('invoice/list');
+        else{
+            console.log('Error during record insertion: ' +err);
+        }       
+    })
 }
+
+router.get('/invoice/list',(req,res)=>{
+    res.json('from List');
+    
+});
 module.exports = router;
